e02382811c0e59dcd1e620ad68cab05e0d732202
Mounahbaieb <mounahbaieb2020@gmail.com>
2024-04-19 22:57:53 +0100

    covoiturage test1

--- src/app/recherche-avancee/recherche-avancee.component.ts
+++ src/app/recherche-avancee/recherche-avancee.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Ride } from '../models/carpooling';
 import { MatDialog } from '@angular/material/dialog';
 import { CarpoolingService } from '../services/carpooling.service';
+import { ToastrService } from 'ngx-toastr';
+import { RidedialogComponent } from '../ridedialog/ridedialog.component';
 
 @Component({
   selector: 'app-recherche-avancee',
@@ -12,8 +14,9 @@ export class RechercheAvanceeComponent implements OnInit {
   rides: Ride[] = [];
   selectedDeparture: string | undefined;
   selectedArrival: string | undefined;
-
-  constructor(private service: CarpoolingService, public dialog: MatDialog) { }
+  
+  selectedDate: Date | undefined = new Date(); // Initialize selectedDa
+  constructor(private service: CarpoolingService, public dialog: MatDialog,private toastr:ToastrService) { }
 
   ngOnInit(): void {
     this.getAllRides();
@@ -29,4 +32,34 @@ export class RechercheAvanceeComponent implements OnInit {
       }
     );
   }
+  search(): void {
+    console.log('Search button clicked');
+    console.log('Selected departure:', this.selectedDeparture);
+    console.log('Selected arrival:', this.selectedArrival);
+    console.log('Selected date:', this.selectedDate);
+
+    if (this.selectedDeparture && this.selectedArrival) {
+      console.log('Searching...');
+      this.toastr.info("En train de rechercher")
+
+      const filteredRides = this.rides.filter(ride => {
+        return ride.departureLocation === this.selectedDeparture &&
+               ride.destinationLocation === this.selectedArrival;
+      });
+
+      console.log('Filtered rides:', filteredRides);
+
+      // Open dialog with filtered rides
+      this.dialog.open(RidedialogComponent, {
+        width: '800px',
+        data: filteredRides
+      });
+    } else {
+      console.log('Please select departure and arrival.');
+      this.toastr.info("choisir lien de depart et lien d'arriver")
+    }
+  }
+
+
+
 }
